f0c8c6e0392bffc85a0260b98965f45c24c05f81
jrelenes <56177974+jrelenes@users.noreply.github.com>
2019-10-07 12:43:29 -0700

    Add files via upload

--- cse360assign2/AddingMachine.java
+++ cse360assign2/AddingMachine.java
@@ -3,28 +3,44 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
-	
-	public AddingMachine () {
+	private String total_string;
+
+	public AddingMachine (int value1) 
+	{
 		total = 0;  // not needed - included for clarity
+		total_string = "0";
+			}
+	
+	public int getTotal () 
+	{
+		System.out.print("\n");
+		System.out.print(total);
+		return total;
 	}
 	
-	public int getTotal () {
-		return 0;
+	public void add (int value)
+	{
+		total=total+value;
+		total_string= total_string + "+" + Integer.toString(value);
 	}
 	
-	public void add (int value) {
+	public void subtract (int value) 
+	{
+		total=total-value;
+		total_string= total_string + "-" + Integer.toString(value);
 		
 	}
+		
+	public String toString () 
+	{
+		System.out.print(total_string);
 	
-	public void subtract (int value) {
-		
-	}
-		
-	public String toString () {
-		return "";
+		return total_string;
+	
 	}
 
 	public void clear() {
 	
 	}
 }
+
